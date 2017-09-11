018cad46447fb9c9aed6fa8d04b5c68914830ef9
AleArioli <alessandro.arioli@italianway.it>
2017-09-12 00:14:45 +0200

    change web server port

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var App = express();
 
 App.use(express.static('public'));
 
-App.use('/', express.static('public/index.html'));
+App.get('/', function(req, res){
+  res.render('index');
+});
 
-App.listen(8080);
+
+/* istanbul ignore next */
+if (!module.parent) {
+  var port = process.env.PORT || 3000;
+  App.listen(port);
+  console.log('Express started on port 3000');
+}
