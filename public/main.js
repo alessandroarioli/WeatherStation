dbffd07ff3513b3669d9ed2309f0bbed27e6e771
AleArioli <alessandro.arioli@italianway.it>
2017-09-12 00:16:44 +0200

    location missing

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el: '.container',
   data: {
+    location: 'Milan, Italy',
     weatherAPI: "https://api.openweathermap.org/data/2.5/weather?q=Milan&APPID=9ee8bea9f54c2263e946dbaae60e7c7e&units=metric",
     status: 'Ready',
     wheatherInfos: [],
