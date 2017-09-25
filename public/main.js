6754915b5c261d709ce7875dc219240dfddc8799
AleArioli <alessandro.arioli@italianway.it>
2017-09-25 22:37:47 +0200

    Refactor the app with components

--- public/main.js
+++ public/main.js
@@ -1,21 +1,93 @@
-var app = new Vue({
-  el: '.container',
+const applicationTemplate = `
+<div class="container">
+  <div class="row">
+  <div class="col-md-12">
+      <div class="weather-card one">
+      <div class="top" v-bind:style="{ 'background-image': 'url(' + badgeImage + ')' }">
+        <div class="wrapper">
+          <h1 class="heading">{{currentInfos.wheaterDescription}}</h1>
+          <h3 class="location">{{location.name}}</h3>
+          <p class="temp">
+            <span class="temp-value" id="temperature">{{currentInfos.main.temp}}</span>
+            <span class="deg">0</span>
+            <a><span class="temp-type">C</span></a>
+          </p>
+          <div class="icon" v-bind:class="[iconClass[0]]">
+            <div v-bind:class="[iconClass[1]]"></div>
+            <div v-bind:class="[iconClass[2]]">
+              <div v-bind:class="[iconClass[3]]"></div>
+            </div>
+            <div v-bind:class="[iconClass[4]]"></div>
+          </div>
+          <div class="flex-div">
+            <span class="flex-one">
+              <u>{{tomorrowDate}}</u> <br>
+              <h3>{{forecast.tomorrowDate}}</h3>
+            </span>
+            <span class="flex-one">
+              <u>{{twoDaysPast}}</u> <br>
+              <h3>{{forecast.twoDaysPast}}</h3>
+            </span>
+            <span class="flex-one">
+              <u>{{threeDaysPast}}</u> <br>
+              <h3>{{forecast.threeDaysPast}}</h3>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="wrapper">
+          <ul class="forecast">
+            <a href="javascript:;"><span class="lnr lnr-chevron-up go-up"></span></a>
+            <li>
+              <span class="date">Humidity</span>
+              <span class="lnr condition">
+                <span class="temp">{{currentInfos.main.humidity}} %</span>
+              </span>
+            </li>
+            <li>
+              <span class="date">Wind speed</span>
+              <span class="lnr condition">
+                <span class="temp">{{currentInfos.wind.speed}} Km/h</span>
+              </span>
+            </li>
+            <li>
+              <span class="date">Pressure</span>
+              <span class="lnr condition">
+                <span class="temp">{{currentInfos.main.pressure}} hPa</span>
+              </span>
+            </li>
+            <li>
+              <span class="date">Pm 2.5</span>
+              <span class="lnr condition">
+                <span class="temp" v-bind:class="pollution.class">{{pollution.pm25}}</span>
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+
+    <div class="row">
+      <div class="mynav">
+        {{status}}
+      </div>
+    </div>
+</div>`
+
+new Vue({
+  el: '#application',
+  template: applicationTemplate,
   data: {
-    location: {
-      id: 6542283,
-      name: 'Milan, Italy'
-    },
+    badgeImage: '',
     weatherAPI: "https://api.openweathermap.org/data/2.5/weather?id=6542283&APPID=9ee8bea9f54c2263e946dbaae60e7c7e&units=metric",
     forecastAPI: "https://api.openweathermap.org/data/2.5/forecast?id=6542283&APPID=9ee8bea9f54c2263e946dbaae60e7c7e",
     pollutionAPI: "https://api.waqi.info/feed/Milan/?token=b683eafa205fc53546270ccd53f071e685b476a4",
-    forecast: {
-      tomorrowDate: '-',
-      twoDaysPast: '-',
-      threeDaysPast: '-'
-    },
     pollution: {
       class: '',
-      pm25: '',
+      pm25: 30,
       pm10: '',
       no2: '',
       co: ''
@@ -34,9 +106,6 @@ var app = new Vue({
       coldest: '#0400ff'
     },
     wheatherInfos: [],
-    iconClass: [
-      '', '', '', '', ''
-    ],
     currentInfos: {
       wheaterDescription: '-',
       main: {
@@ -47,6 +116,32 @@ var app = new Vue({
       wind: {
         speed: 0
       }
+    },
+    location: {
+      id: 6542283,
+      name: 'Milan, Italy'
+    },
+    iconClass: [
+      '', '', '', '', ''
+    ],
+    forecast: {
+      tomorrowDate: '-',
+      twoDaysPast: '-',
+      threeDaysPast: '-'
+    }
+  },
+  computed: {
+    tomorrowDate: function() {
+      var today = new Date();
+      return (today.getDate() + 1) + '/' + (today.getMonth() + 1)
+    },
+    twoDaysPast: function() {
+      var today = new Date();
+      return (today.getDate() + 2) + '/' + (today.getMonth() + 1)
+    },
+    threeDaysPast: function() {
+      var today = new Date();
+      return (today.getDate() + 3) + '/' + (today.getMonth() + 1)
     }
   },
   methods: {
@@ -70,21 +165,19 @@ var app = new Vue({
           })
     },
     setBackgroundImage: function() {
-      var badgeContainer = document.getElementById('badge');
       var time = new Date().getHours();
+      var image;
 
       if (this.currentInfos.wheaterDescription.includes('rain')) {
-        var rainingImage = this.images.rain[Math.floor(Math.random() * this.images.rain.length)]
-        badgeContainer.style.backgroundImage = "url('" + rainingImage + "')"
+        image = this.images.rain[Math.floor(Math.random() * this.images.rain.length)]
       } else {
           if (time > 5 && time < 18) {
-          var sunnyImage = this.images.sunny[Math.floor(Math.random() * this.images.sunny.length)]
-          badgeContainer.style.backgroundImage = "url('" + sunnyImage + "')"
+          image = this.images.sunny[Math.floor(Math.random() * this.images.sunny.length)]
         } else {
-          var nightImage = this.images.nights[Math.floor(Math.random() * this.images.nights.length)]
-          badgeContainer.style.backgroundImage = "url('" + nightImage + "')"
+          image = this.images.nights[Math.floor(Math.random() * this.images.nights.length)]
         }
       }
+      this.badgeImage = image
     },
     setTemperatureColor: function() {
       var temp = parseFloat(document.getElementById('temperature').innerText);
@@ -182,20 +275,6 @@ var app = new Vue({
       }
       this.currentInfos.wheaterDescription = descr
       this.setAnimatedIcon();
-    }
-  },
-  computed: {
-    tomorrowDate: function() {
-      var today = new Date();
-      return (today.getDate() + 1) + '/' + (today.getMonth() + 1)
-    },
-    twoDaysPast: function() {
-      var today = new Date();
-      return (today.getDate() + 2) + '/' + (today.getMonth() + 1)
-    },
-    threeDaysPast: function() {
-      var today = new Date();
-      return (today.getDate() + 3) + '/' + (today.getMonth() + 1)
     }
   }
 });
